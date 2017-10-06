07252438e85e421ebd09bab6b97abcd8cf3f3c8b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-06 01:25:06 +0000

    automatically backed up by learn

--- db/migrate/005_create_character.rb
+++ db/migrate/005_create_character.rb
@@ -1,6 +1,6 @@
 class CreateCharacter < ActiveRecord::Migration[4.2]
   def change
-    create_table :chracter do |c|
+    create_table :character do |c|
       c.string :name
       c.integer :show_id
     end
