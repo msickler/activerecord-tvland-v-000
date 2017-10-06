86aaa719f47783ff08720a680f1df58795bff7ae
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-06 01:33:42 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ActiveRecord::Base
-  has_many :character
-  has_many   :actors, through: :character
+  has_many :characters
+  has_many  :actors, through: :characters
   belongs_to :network
 end
