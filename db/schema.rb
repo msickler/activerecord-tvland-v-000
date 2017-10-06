baf0c838032e9f6436ffd03a1e8dd90e894d690b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-06 01:22:20 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,22 +1,22 @@
 ActiveRecord::Schema.define(version: 9) do
- 
+
   create_table "actors", force: :cascade do |t|
      t.string "first_name"
      t.string "last_name"
    end
- 
+
    create_table "characters", force: :cascade do |t|
      t.string  "name"
      t.integer "show_id"
      t.integer "actor_id"
      t.string  "catchphrase"
    end
- 
+
    create_table "networks", force: :cascade do |t|
      t.string  "call_letters"
      t.integer "channel"
   end
- 
+
    create_table "shows", force: :cascade do |t|
      t.string  "name"
      t.integer "network_id"
@@ -24,5 +24,5 @@ ActiveRecord::Schema.define(version: 9) do
      t.string  "day"
      t.string  "season"
    end
- 
+
  end
